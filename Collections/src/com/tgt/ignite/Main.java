60ee6bf699c3ba10cb2c43b59b206b594af27f07
Gautham U <gautham.umapathy@gmail.com>
2020-04-09 17:10:09 +0530

    Update Main.java

--- Collections/src/com/tgt/ignite/Main.java
+++ Collections/src/com/tgt/ignite/Main.java
@@ -1,9 +1,6 @@
 package com.tgt.ignite;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
 
@@ -26,9 +23,34 @@ public class Main {
         igniteMembers.add(new IgniteMembers("Divya","TGT",25, divyaSkillSet));
 
         for (IgniteMembers i : igniteMembers){
-            
+            if (i.getSkillSet().contains("sql"))
                 System.out.println(i.toString());
         }
+
+
+        List<IgniteMembers> cloudteam = new ArrayList<>();
+        cloudteam.add(new IgniteMembers("Amit", "TGT", 26, gauthamSkillSet));
+        cloudteam.add(new IgniteMembers("Arun","TGT", 27, divyaSkillSet));
+
+        Map<String,List<IgniteMembers>> mem = new HashMap<>();
+        mem.put("DOJO",igniteMembers);
+        mem.put("Cloud",cloudteam);
+
+
+
+
+        for (String dept: mem.keySet()){
+            System.out.println( "Department Name: "+ dept);
+            System.out.println("-----------------------------------");
+            for(IgniteMembers i: mem.get(dept))
+                System.out.println(i.toString());
+            System.out.println("-----------------------------------");
+        }
+
+
+
+
+
 
     }
 }
